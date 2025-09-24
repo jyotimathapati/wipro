316ed7818f3f36fd42a300f58f3b96793b37e9f9
jyotimathapati <139350666+jyotimathapati@users.noreply.github.com>
2025-09-24 14:52:50 +0530

    Update App.java

--- src/main/java/micro/App.java
+++ src/main/java/micro/App.java
@@ -6,6 +6,6 @@ package micro;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        
     }
 }
